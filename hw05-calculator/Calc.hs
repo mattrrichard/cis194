993500b26a0b011b39125e5007e5b7cfb44cd8be
Matt Richard <matt@richardsoftware.com>
2016-02-16 01:04:16 -0600

    add stackvm 'compiler' and HasVars exercises

--- hw05-calculator/Calc.hs
+++ hw05-calculator/Calc.hs
@@ -4,6 +4,9 @@ module Calc where
 
 import Parser
 import StackVM
+import qualified Data.Map as M
+import qualified ExprT as E
+import Control.Monad
 
 parseExpr :: Expr a => String -> Maybe a
 parseExpr = parseExp lit add mul
@@ -56,3 +59,48 @@ instance Expr Program where
   lit x = [PushI x]
   add x y = x ++ y ++ [Add]
   mul x y = x ++ y ++ [Mul]
+
+
+compile :: String -> Maybe Program
+compile =
+  parseExpr
+
+compileAndRun :: String -> Either String StackVal
+compileAndRun str =
+  case parseExpr str of
+    Just p -> stackVM p
+    Nothing -> Left "Failed to parse program"
+
+
+class HasVars a where
+  var :: String -> a
+
+data VarExprT
+  = Var String
+  | VLit Integer
+  | VAdd VarExprT VarExprT
+  | VMul VarExprT VarExprT
+  deriving (Show, Eq)
+
+
+instance HasVars VarExprT where
+  var = Var
+
+instance Expr VarExprT where
+  lit = VLit
+  add = VAdd
+  mul = VMul
+
+instance HasVars (M.Map String Integer -> Maybe Integer) where
+  var = M.lookup
+
+instance Expr (M.Map String Integer -> Maybe Integer) where
+  lit = const . Just
+  add x y m = liftM2 (+) (x m) (y m)
+  mul x y m = liftM2 (*) (x m) (y m)
+
+
+withVars :: [(String, Integer)]
+  -> (M.Map String Integer -> Maybe Integer)
+  -> Maybe Integer
+withVars vs expr = expr $ M.fromList vs
