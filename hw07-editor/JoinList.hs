381d617456174842667e22da259beea67aa41e97
Matt Richard <matt@richardsoftware.com>
2016-02-17 23:16:22 -0600

    implement dropJ and takeJ

--- hw07-editor/JoinList.hs
+++ hw07-editor/JoinList.hs
@@ -38,14 +38,28 @@ indexJ i (Append _ l r)
   | otherwise = indexJ (i - lSize) r
   where lSize = sizedTag l
 
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ _ Empty = Empty
+dropJ n jl
+  | n <= 0 = jl
+  | n >= sizedTag jl = Empty
+dropJ n (Append _ l r)
+  | n >= lSize = dropJ (n-lSize) r
+  | otherwise = dropJ n l +++ r
+  where lSize = sizedTag l
+-- this next case is only possible if `Single` nodes can have size > 1,
+-- and since no real way to split them, just kill the whole node
+dropJ _ (Single _ _) = Empty
 
-(!!?) :: [a] -> Int -> Maybe a
-[]     !!? _        = Nothing
-_      !!? i | i < 0 = Nothing
-(x:_)  !!? 0     = Just x
-(_:xs) !!? i    = xs !!? (i-1)
 
-jlToList :: JoinList m a -> [a]
-jlToList Empty            = []
-jlToList (Single _ a)     = [a]
-jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ _ Empty = Empty
+takeJ n jl
+  | n <= 0 = Empty
+  | n >= sizedTag jl = jl
+takeJ _ jl@(Single _ _) = jl
+takeJ n (Append _ l r)
+  | lSize < n = l +++ takeJ (n - lSize) r
+  | otherwise = takeJ n l
+  where lSize = sizedTag l
+
